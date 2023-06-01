4f8d97459a14d43086815c1f6822903aee7eb175
cotterma <matteo.cottereau@live.fr>
2023-06-01 17:25:52 +0200

    Right backend url

--- frontend/script/config.js
+++ frontend/script/config.js
@@ -1,4 +1,4 @@
-const back = "https://squareinvest.osc-fr1.scalingo.io";
+const back = "https://squareinvest1-sartaford1203.b4a.run";
 const admins = ["squareinvest38@yahoo.fr", "matteo.cottereau@live.fr"]
 
 export { back, admins };
