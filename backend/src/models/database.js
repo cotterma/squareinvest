41557b0a31536a40a673d338a757b557ee417030
cotterma <matteo.cottereau@live.fr>
2023-06-01 15:55:47 +0200

    Trying without DB link

--- backend/src/models/database.js
+++ backend/src/models/database.js
@@ -3,10 +3,10 @@ require('mandatoryenv').load(['DB'])
 const { DB } = process.env
 
 const Sequelize = require('sequelize')
-// const db = new Sequelize({
-//   dialect: 'sqlite',
-//   storage: DB,
-//   logging: (...msg) => console.log(msg)
-// })
-const db = new Sequelize(DB)
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: DB,
+  logging: (...msg) => console.log(msg)
+})
+// const db = new Sequelize(DB)
 module.exports = db
