63318e1c02112bf227dbf2a975943dccdfc49ebd
cotterma <119701569+cotterma@users.noreply.github.com>
2024-10-04 19:31:10 +0200

    Update config.js

--- frontend/script/config.js
+++ frontend/script/config.js
@@ -1,4 +1,4 @@
-const back = "https://squareweb1-b1kt29g7.b4a.run";
+const back = "https://squareinvestweb-p1bdksbu.b4a.run";
 const admins = ["squareinvest38@yahoo.fr", "matteo.cottereau@live.fr"]
 
 export { back, admins };
