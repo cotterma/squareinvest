175f2c981bfc0b6c6064225408b883cef3d00ba4
cotterma <119701569+cotterma@users.noreply.github.com>
2024-10-04 18:43:31 +0200

    change back service

--- frontend/script/config.js
+++ frontend/script/config.js
@@ -1,4 +1,4 @@
-const back = "https://squareweb.adaptable.app";
+const back = "https://squareweb1-b1kt29g7.b4a.run";
 const admins = ["squareinvest38@yahoo.fr", "matteo.cottereau@live.fr"]
 
 export { back, admins };
